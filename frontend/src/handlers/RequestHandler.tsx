ba51fba12221c654085fc5012601666db0346635
Wei-Xinn <E0544325@u.nus.edu>
2023-09-19 17:24:54 +0800

    feat: add template methods

---
+++ frontend/src/handlers/RequestHandler.tsx
@@ -0,0 +1,10 @@
+import { QuestionString } from "../Commons";
+
+class RequestHandler {
+  static getQuestions() { }
+  static deleteQuestion(id: string) { }
+  static updateQuestion(question: QuestionString) { }
+  static addQuestion(question: QuestionString) { }
+}
+
+export default RequestHandler;
