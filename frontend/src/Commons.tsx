a3abd99ab9a9ceaf847b8e0dc70f243d63585190
Wei-Xinn <E0544325@u.nus.edu>
2023-11-12 20:39:03 +0800

    chore: remove unused interfaces

--- frontend/src/Commons.tsx
+++ frontend/src/Commons.tsx
@@ -1,5 +1,3 @@
-import { Socket } from 'socket.io-client';
-
 export interface QuestionString {
   id: string,
   title: string,
@@ -18,70 +16,7 @@ export const emptyQuestionString: QuestionString = {
   link: ''
 }
 
-export interface UserDataString {
-  id: string;
-  username: string;
-  email: string;
-  role: string;
-}
-
-export interface HistoryDataString {
-  userId: string;
-  attempt: {
-    questionId: string;
-    timestamp: string;
-  };
-  complexity: string;
-}
-
-export interface MatchingString {
-  complexity: string;
-  categories: string[]; // To be changed into [] after assignment 5
-}
-
-export const emptyMatchingString: MatchingString = {
-  complexity: '',
-  categories: [], // To be changed into [] after assignment 5
-}
-
-export interface MatchDataString {
-  user_id: string;
-  other_user: string;
-  question: QuestionString;
-  room_id: number;
-}
-
 export interface NotificationOptions {
   message: string;
   type: 'success' | 'error'
-}
-
-export interface Attempt {
-  questionId: string;
-  timestamp: string;
-};
-
-export interface ChatMessage {
-  sender: string;
-  text: string;
-};
-
-export interface ChatProps {
-  messages: ChatMessage[];
-  newMessage: string;
-  onNewMessageChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  onSendMessage: () => void;
-};
-
-export interface CanvasProps {
-  socket?: Socket;
-}
-
-export interface HistoryResponseString {
-  userId: string;
-  total: string;
-  attempts: [{
-    questionId: string,
-    timestamp: string
-  }]
 }
